cd58e43c3b9d76861d8f19e00b576618b4c4d66e
Matt Delhey <matt.delhey@gmail.com>
2013-07-07 19:53:54 -0400

    Test submission

--- 1.R
+++ 1.R
@@ -1,4 +1,13 @@
+library(caret)
+
 test <- read.csv("Data/test.csv")
 train <- read.csv("Data/train.csv")
 submit <- read.csv("Data/sampleSubmission.csv")
 
+train$ACTION <- factor(train$ACTION)
+
+rf <- train(ACTION ~ ., data = train, method = "rf")
+
+submit$Action2 <- predict(rf, test)
+
+write.table(submit, "Submissions/test_submission.csv", sep = ",", row.names = FALSE)
